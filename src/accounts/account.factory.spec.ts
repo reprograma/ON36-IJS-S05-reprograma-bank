3ac55ffb9538970f045ce886d5819585f702e209
Lais Frigerio <lais.frigerio@nubank.com.br>
2024-08-03 14:30:05 -0300

    Fixing createAccount parameters

--- src/accounts/account.factory.spec.ts
+++ src/accounts/account.factory.spec.ts
@@ -1,6 +1,7 @@
 import { AccountFactory } from './account.factory';
 import { AccountType } from './account-type.enum';
 import { CheckingAccount, SavingsAccount } from './account.entity';
+import { Client } from '../clients/client.entity';
 
 /*
  * Jest:
@@ -8,9 +9,13 @@ import { CheckingAccount, SavingsAccount } from './account.entity';
  */
 
 describe('Account Factory', () => {
+  const client = new Client('John', 'Rua caxias', '12345678900');
   test('should create a checking account', () => {
     const accountFactory = new AccountFactory();
-    const retornado = accountFactory.createAccount(AccountType.Checking);
+    const retornado = accountFactory.createAccount(
+      AccountType.Checking,
+      client,
+    );
 
     if (retornado instanceof CheckingAccount) {
       expect(retornado.balance).toBe(0);
@@ -23,6 +28,7 @@ describe('Account Factory', () => {
     const accountFactory = new AccountFactory();
     const retornado = accountFactory.createAccount(
       AccountType.Savings,
+      client,
     ) as SavingsAccount;
 
     expect(retornado.balance).toBe(0);
--- src/accounts/account.factory.ts
+++ src/accounts/account.factory.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { AccountType } from './account-type.enum';
 import { CheckingAccount, SavingsAccount } from './account.entity';
+import { Client } from 'src/clients/client.entity';
 
 @Injectable()
 export class AccountFactory {
-  createAccount(typeAccount: AccountType): CheckingAccount | SavingsAccount {
+  createAccount(
+    typeAccount: AccountType,
+    client: Client,
+  ): CheckingAccount | SavingsAccount {
     switch (typeAccount) {
       case AccountType.Checking:
         const checkingAccount = new CheckingAccount();
         checkingAccount.balance = 0;
         checkingAccount.overdraftLimit = 500;
+        checkingAccount.client = client;
         checkingAccount.typeAccount = AccountType.Checking;
         return checkingAccount;
       case AccountType.Savings:
@@ -17,6 +22,7 @@ export class AccountFactory {
         savingsAccount.balance = 0;
         savingsAccount.taxRate = 0.02;
         savingsAccount.typeAccount = AccountType.Savings;
+        savingsAccount.client = client;
         return savingsAccount;
       default:
         throw new Error('Invalid account type');
